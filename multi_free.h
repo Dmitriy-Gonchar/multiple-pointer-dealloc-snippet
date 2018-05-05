920c0c60d2f2c30b4bc6bda2d866fb74b8824f75
Jesus <hlamspam@ya.ru>
2018-05-05 08:53:31 +0700

    free() patch

---
+++ multi_free.h
@@ -0,0 +1,30 @@
+//
+//  multi_free.h
+//
+//  Created by Dmitriy Gonchar on 05.05.18.
+//  Copyright © 2018 Dmitriy Gonchar. All rights reserved.
+//
+
+#ifndef multi_free_h
+#define multi_free_h
+
+#include <stdlib.h>
+
+#define FREE(...) multi_free(__VA_ARGS__, NULL);
+
+void multi_free(void *first, ...)
+{
+	void *cleaned_mem = first;
+	va_list aList;
+	va_start(aList, first);
+	do
+	{
+		free(cleaned_mem);
+		cleaned_mem = va_arg(aList, void *);
+	} while (cleaned_mem);
+	va_end(aList);
+}
+
+#define free FREE
+
+#endif /* multi_free_h */
